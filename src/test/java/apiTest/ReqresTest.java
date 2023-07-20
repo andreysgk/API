bed74d6e25eccc6ce33ee2e2adbb14c8f6bd7997
pupaandlupa2213 <andrey_sgk@mail.ru>
2023-07-20 17:53:05 +0300

    api

--- src/test/java/apiTest/ReqresTest.java
+++ src/test/java/apiTest/ReqresTest.java
@@ -2,7 +2,10 @@ package apiTest;
 import dev.failsafe.internal.util.Assert;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pojos.Registration;
+import pojos.SuccessReg;
 import pojos.UserData;
 
 import java.util.List;
@@ -12,6 +15,7 @@ import static io.restassured.RestAssured.given;
 public class ReqresTest{
     private static final String URL = "https://reqres.in/";
     @Test
+    @DisplayName("GET")
     public void getUsers(){
         Specifications.installSpecification(Specifications.requestSpecification(URL),Specifications.responseSpecificationOK200());
         List<UserData> users = given()
@@ -21,7 +25,23 @@ public class ReqresTest{
                 .extract().body().jsonPath().getList("data", UserData.class);
         users.forEach(x-> Assertions.assertTrue(x.getAvatar().contains(x.getId().toString())));
 //        users.forEach(x-> Assertions.assertTrue(x.getEmail().endsWith("reqres.in")));
+    }
 
+    @Test
+    @DisplayName("POST")
+    public void successRegistration(){
+        Specifications.installSpecification(Specifications.requestSpecification(URL),Specifications.responseSpecificationOK200());
+        Integer id = 4;
+        String token = "QpwL5tke4Pnpja7X4";
+        Registration user = new Registration("eve.holt@reqres.in", "pistol");
+        SuccessReg successReg = given()
+                .body(user)
+                .when()
+                .post("api/register")
+                .then().log().all()
+                .extract().as(SuccessReg.class);
+        Assertions.assertEquals(id, successReg.getId());
+        Assertions.assertEquals(token, successReg.getToken());
     }
 
 }
---
+++ src/test/java/pojos/Registration.java
@@ -0,0 +1,18 @@
+package pojos;
+
+public class Registration {
+    private String email;
+    private String password;
+    public Registration(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
