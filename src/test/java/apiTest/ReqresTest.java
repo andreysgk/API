8c8d4ccd2f8eed6fac5b49621b25bd79ef1358a7
pupaandlupa2213 <andrey_sgk@mail.ru>
2023-07-20 19:08:30 +0300

    api

--- src/test/java/apiTest/ReqresTest.java
+++ src/test/java/apiTest/ReqresTest.java
@@ -1,14 +1,11 @@
 package apiTest;
-import dev.failsafe.internal.util.Assert;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pojos.Registration;
-import pojos.SuccessReg;
-import pojos.UserData;
+import pojos.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 
@@ -20,7 +17,8 @@ public class ReqresTest{
         Specifications.installSpecification(Specifications.requestSpecification(URL),Specifications.responseSpecificationOK200());
         List<UserData> users = given()
 //                .contentType(ContentType.JSON)
-                .when().get("api/users?page=2")
+                .when()
+                .get("api/users?page=2")
                 .then().log().all()
                 .extract().body().jsonPath().getList("data", UserData.class);
         users.forEach(x-> Assertions.assertTrue(x.getAvatar().contains(x.getId().toString())));
@@ -40,8 +38,40 @@ public class ReqresTest{
                 .post("api/register")
                 .then().log().all()
                 .extract().as(SuccessReg.class);
+        Assertions.assertNotNull(successReg.getId());
+        Assertions.assertNotNull(successReg.getToken());
         Assertions.assertEquals(id, successReg.getId());
         Assertions.assertEquals(token, successReg.getToken());
+    }
+
+    @Test
+    @DisplayName("POST")
+    public void unSuccessRegistration(){
+        Specifications.installSpecification(Specifications.requestSpecification(URL),Specifications.responseSpecificationError400());
+        Registration user = new Registration("sydney@fife","");
+        UnSuccessReg unSuccessReg = given()
+                .body(user)
+                .when()
+                .post("api/register")
+                .then().log().all()
+                .extract().as(UnSuccessReg.class);
+        Assertions.assertEquals("Missing password", unSuccessReg.getError());
+    }
+
+    @Test
+    @DisplayName("GET, отсортированы ли года?")
+    public void getResourсes(){
+        Specifications.installSpecification(Specifications.requestSpecification(URL),Specifications.responseSpecificationOK200());
+        List<Resourсes> resourсes = given()
+                .when()
+                .get("api/unknown")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data", Resourсes.class);
+        List<Integer> years = resourсes.stream().map(Resourсes::getYear).collect(Collectors.toList());
+        List<Integer> sortYears = years.stream().sorted().collect(Collectors.toList());
+        Assertions.assertEquals(sortYears, years);
+//        System.out.println(years);
+//        System.out.println(sortYears);
     }
 
 }
